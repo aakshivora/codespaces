22c6596b3aa1f0d202f3f0ea030fb6f1fc8a5be0
aakshivora <aakshivora1101@gmail.com>
2023-11-01 23:35:21 +0530

    Initial commit

--- server.js
+++ server.js
@@ -62,5 +62,5 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 1000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
